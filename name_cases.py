8b1e9d392347b8314028d0467e9cdaee7c430d3a
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2024-12-02 13:47:35 +0900

    12/02 Python学習

---
+++ name_cases.py
@@ -0,0 +1,21 @@
+Kohei = "こんにちはEric、今日はPythonを学びますか？"
+print(Kohei)
+
+name = "shimizu kohei"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+message = "誰かが言った”七転び八起き”と。"
+print(message)
+
+name2 = "\tshimizu kohei"
+print(name2)
+name2 = "\t\nshimizu kohei"
+print(name2)
+name2 = "' shimizu kohei '"
+print(name2.lstrip())
+name2 = ' shimizu kohei '
+print(name2.rstrip())
+name2 = ' shimizu kohei '
+print(name2.strip())
