67de172becacf13d34c16e47dd4dda08d148614a
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2024-12-04 10:34:21 +0900

    フォルダ整理と第4章着手

---
+++ 第4章 リストを操作する/magicians.py
@@ -0,0 +1,18 @@
+# ループ処理
+magicians = ['alice', 'david', 'carolina']
+
+# magiciansリストから、最初の一つを取り出し、magician変数に格納（以下、繰り返し）
+for magician in magicians:
+    print(magician)
+
+# 練習問題4-1
+pizzas = ['a', 'bb', 'ccc']
+for pizza in pizzas:
+    print(pizza)
+
+# 練習問題4-2
+animals = ['dog', 'cat', 'sheep']
+for animal in animals:
+    print(f"{animal.title()}は素晴らしい動物です")
+    print("あと、とてもかわいい\n")
+print("この動物たちはとても素晴らしいです！")
