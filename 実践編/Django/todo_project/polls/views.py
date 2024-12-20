c195136d7066736a620800ab284364ee24664003
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2024-12-20 09:45:44 +0900

    homeページの作成

--- 実践編/Django/todo_project/polls/views.py
+++ 実践編/Django/todo_project/polls/views.py
@@ -2,6 +2,9 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     		return HttpResponse("Hello, world!")
+def home(request):
+    		return render(request, 'polls/home.html')
