9c204f65111ff5b40395e8f18fb51a7cafc5c590
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2024-12-20 11:41:50 +0900

    モデルまで実装

--- 実践編/Django/todo_project/polls/urls.py
+++ 実践編/Django/todo_project/polls/urls.py
@@ -6,4 +6,7 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
+    path('mypage/', views.mypage, name='mypage'),
+    path('form/', views.form, name='form'),
+    path('confirmform/', views.confirmform, name='confirmform'),
 ]
--- 実践編/Django/todo_project/polls/views.py
+++ 実践編/Django/todo_project/polls/views.py
@@ -8,3 +8,23 @@ def index(request):
     		return HttpResponse("Hello, world!")
 def home(request):
     		return render(request, 'polls/home.html')
+def mypage(request):
+    		return render(request, 'polls/mypage.html')
+def home(request):
+    # 辞書データを用意し渡す
+    data = {
+        'name':"suzuki taro",
+        'address':'Tokyo'
+        }
+    return render(request, 'polls/home.html', data)
+
+# polls/formで呼び出される関数
+def form(request):
+    return render(request, 'polls/form.html')
+
+# polls/confirmformで呼び出される関数
+def confirmform(request):
+    # form method='POST'で送信されたデータを取得
+    message = request.POST['message']
+    data = {'message':message}
+    return render(request, 'polls/confirmform.html', data)
