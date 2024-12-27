51a3961a19f04fe21699718d5605b587469a5b54
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2024-12-27 15:44:12 +0900

    エイリアンを出現させて倒すまで

--- game/settings.py
+++ game/settings.py
@@ -8,11 +8,20 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
-        self.ship_speed =1.5
 
         # 弾の設定
-        self.bullet_speed = 1.0 
-        self.bullet_width = 3 
+        self.bullet_speed = 1.5 
+        self.bullet_width = 300
         self.bullet_height = 15 
-        self.bullet_color = (60, 60, 60) 
+        self.bullet_color = (255, 0, 0) 
         self.bullets_allowed = 3
+
+        # エイリアンの設定
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # 艦隊の移動方向を表し、１は右、ー１は左に移動することを表す
+        self.fleet_direction = 1
+
+        # ship settings
+        self.ship_speed =1.5
+        self.ship_limit = 3
