ecc65a098919307758cb18b3547b4d4b47812416
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2025-01-09 09:07:38 +0900

    ベンダー資格勉強

--- 実践編/memo.py
+++ 実践編/memo.py
@@ -1,9 +1,18 @@
-c
-# d -> b
-c
-b
-b
-a
-a
-b
-a
+# b
+# ?
+# c
+# d
+# c
+# d
+# d
+# d
+# c
+# c
+
+
+def num(value):
+    return value
+
+value1 = num(5*0) and num(3/3) and num(4/2)
+value2 = num(5*0) or num(3/3) or num(4/2)
+print(value1, value2)
